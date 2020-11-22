b4ec74e4f6c9dc4bfbc6b6d7c4435d666c41461c
Adrian Apodaca <aapodaca@gmail.com>
2020-11-21 21:11:48 -0800

    begin structuring redux cycle

---
+++ frontend/src/components/reducers/users_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_USERS, RECEIVE_USER, RECEIVE_USER_ERRORS } from '../actions/user_actions'
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+        case RECEIVE_USERS:
+            return Object.assign({}, state, action.users);
+        case RECEIVE_USER:
+            return Object.assign({}, state, {[action.user.id]: action.user});
+
+        default:
+            return state;
+        
+    }
+};
+
+export default usersReducer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from './components/store/store.js'
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener("DOMContentLoaded", () => {
+  let store = configureStore()
+
+  // authentication snippet 
+
+    // if (window.currentUser) {
+    //     const preloadedState = {
+    //         session: { id: window.currentUser.id },
+    //         entities: {
+    //             users: { [window.currentUser.id]: window.currentUser }
+    //         }
+    //     };
+    //     store = configureStore(preloadedState);
+    //     delete window.currentUser;
+    // } else {
+    //     store = configureStore();
+    // }
+    ReactDOM.render(
+      <React.StrictMode>
+        <App store={store}/>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
