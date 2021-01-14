40115220f55fa980659088391352fb13acfa69fd
Adrian Apodaca <aapodaca@gmail.com>
2021-01-13 19:04:34 -0800

    update register reducer

--- frontend/reducers/register_reducer.js
+++ frontend/reducers/register_reducer.js
@@ -1,1 +1,19 @@
-import {}
+import { UPDATE_REGISTER, UPDATE_USER_PROFILE} from '../actions/register_actions';
+
+
+const registerReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case UPDATE_REGISTER:
+            newState = {...state};
+            Object.keys(action.registerObject).forEach((key, idx) => {
+                newState[key] = Object.values(action.registerObject)[idx];
+            })
+
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default registerReducer;
