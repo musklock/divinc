6e0ccd4749476accbea7eaf86c7ebb3bc26cef11
Adrian Apodaca <aapodaca@gmail.com>
2021-01-13 19:04:20 -0800

    update register actions

--- frontend/actions/current_otb_sample_history_actions.js
+++
@@ -1,8 +0,0 @@
-export const SET_CURRENT_OTB_SAMPLE_HISTORIES = "SET_CURRENT_OTB_SAMPLE_HISTORIES";
-
-export const setCurrentOTBSampleHistories = currentOTBSampleHistories => {
-    return {
-    type: SET_CURRENT_OTB_SAMPLE_HISTORIES,
-    currentOTBSampleHistories
-    }
-}
--- frontend/actions/register_actions.js
+++ frontend/actions/register_actions.js
@@ -1,8 +1,7 @@
-import * as OTBSampleAPIUtil from '../../util/otb_sample_api_util.js'
+// import * as OTBSampleAPIUtil from '../../util/otb_sample_api_util.js'
 
 export const UPDATE_REGISTER = 'UPDATE_REGISTER_OBJECT_OBJECT';
 export const UPDATE_USER_PROFILE= "UPDATE_USER_PROFILE";
-export const REMOVE_OTB_SAMPLE = "REMOVE_OTB_SAMPLE";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS"
 export const CLEAR_ERRORS = 'CLEAR_ERRORS'
 
@@ -22,58 +21,73 @@ const updateRegisterObject = registerObject => {
 
 }
 
-const updateProfile = userObject => {
+const updateProfile = user => {
     type: UPDATE_USER_PROFILE,
-    userObject
+    user
 }
-const receiveOTBSamples = OTBSamples => {
+
+
+export const updateRegistrationProfileObject = (registrationObject) => dispatch => {
+   dispatch(updateRegisterObect(registrationObject)), err => dispatch(receiveErrors(err.response.JSON))
+}  
+
+export const updateProfile = userObject => dispatch => {
+    dispatch(updateProfile(userObject)), err => dispatch(receiveErrors(err.response.JSON))
+}
+
+
+
+
+// const receiveOTBSamples = OTBSamples => {
     
- return {   type: RECEIVE_OTB_SAMPLES,
-    OTBSamples
-     }
-}
+//  return {   type: RECEIVE_OTB_SAMPLES,
+//     OTBSamples
+//      }
+// }
 
-const receiveOTBSample = OTBSample => ({
-    type: RECEIVE_OTB_SAMPLE,
-    OTBSample
-})
+// const receiveOTBSample = OTBSample => ({
+//     type: RECEIVE_OTB_SAMPLE,
+//     OTBSample
+// })
 
-const removeOTBSample = OTBSampleId => ({
-    type: REMOVE_OTB_SAMPLE,
-    OTBSampleId
-})
+// const removeOTBSample = OTBSampleId => ({
+//     type: REMOVE_OTB_SAMPLE,
+//     OTBSampleId
+// })
 
-export const fetchOTBSamples = () => dispatch => (
-    OTBSampleAPIUtil.fetchOTBSamples()
-        .then(OTBSamples => (dispatch(receiveOTBSamples(OTBSamples))
-        ), err => (
-            dispatch(receiveErrors(err.responseJSON))
-        ))
-)
 
-export const fetchOTBSample = id => dispatch => (
-    OTBSampleAPIUtil.fetchOTBSample(id)
-        .then(OTBSample => (dispatch(receiveAirTravel(OTBSample))
-        ), err => (
-            dispatch(receiveErrors(err.responseJSON))
-        ))
-)
 
-export const createOTBSample = OTBSample => dispatch => (
-    OTBSampleAPIUtil.createOTBSample(OTBSample)
-        .then(OTBSample => (dispatch(receiveOTBSample(OTBSample))
-        ), err => (
-            dispatch(receiveErrors(err.responseJSON))
-        ))
-)
-export const updateOTBSample = OTBSample => dispatch => (
-    OTBSampleAPIUtil.updateOTBSample(OTBSample)
-        .then(OTBSample => (dispatch(receiveOTBSample(OTBSample))
-        ), err => (
-            dispatch(receiveErrors(err.responseJSON))
-        ))
-)
-export const deleteOTBSample = OTBSampleId => dispatch => (
-    OTBSampleAPIUtil.deleteOTBSample(OTBSampleId)
-        .then(() => dispatch(removeOTBSample(OTBSampleId)))
-)
+// export const fetchOTBSamples = () => dispatch => (
+//     OTBSampleAPIUtil.fetchOTBSamples()
+//         .then(OTBSamples => (dispatch(receiveOTBSamples(OTBSamples))
+//         ), err => (
+//             dispatch(receiveErrors(err.responseJSON))
+//         ))
+// )
+
+// export const fetchOTBSample = id => dispatch => (
+//     OTBSampleAPIUtil.fetchOTBSample(id)
+//         .then(OTBSample => (dispatch(receiveAirTravel(OTBSample))
+//         ), err => (
+//             dispatch(receiveErrors(err.responseJSON))
+//         ))
+// )
+
+// export const createOTBSample = OTBSample => dispatch => (
+//     OTBSampleAPIUtil.createOTBSample(OTBSample)
+//         .then(OTBSample => (dispatch(receiveOTBSample(OTBSample))
+//         ), err => (
+//             dispatch(receiveErrors(err.responseJSON))
+//         ))
+// )
+// export const updateOTBSample = OTBSample => dispatch => (
+//     OTBSampleAPIUtil.updateOTBSample(OTBSample)
+//         .then(OTBSample => (dispatch(receiveOTBSample(OTBSample))
+//         ), err => (
+//             dispatch(receiveErrors(err.responseJSON))
+//         ))
+// )
+// export const deleteOTBSample = OTBSampleId => dispatch => (
+//     OTBSampleAPIUtil.deleteOTBSample(OTBSampleId)
+//         .then(() => dispatch(removeOTBSample(OTBSampleId)))
+// )
